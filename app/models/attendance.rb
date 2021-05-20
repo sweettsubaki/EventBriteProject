ab38547e28c25d221a41b5232921fca03e7567a4
sweettsubaki <genmashika@hotmail.fr>
2021-05-20 11:29:43 +0200

    2nd commit

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,2 +1,8 @@
 class Attendance < ApplicationRecord
+    after_create :event_attendance_send
+
+    def event_attendance_send
+      UserMailer.event_attendance_email(self).deliver_now
+    end
+  
 end
